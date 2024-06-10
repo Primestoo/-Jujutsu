41273a84c3c6c20f00fc0a684345769a9b9b1247
Primestoo <161174803+Primestoo@users.noreply.github.com>
2024-06-10 04:43:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function redirectToHome() {
-  window.location.href = "index.html";
+let darkModeEnabled = false;
+
+function redirectToDiscord() {
+  window.location.href = "https://discord.com/invite/dc5WxnmT";
 }
 
 function redirectToRules() {
   window.location.href = "rules.html";
 }
 
-function redirectToDiscord() {
-  window.location.href = "https://discord.com/invite/dc5WxnmT";
+function toggleDarkMode() {
+  const stylesheet = document.getElementById('stylesheet');
+  if (!darkModeEnabled) {
+    stylesheet.href = 'styles2.css';
+    darkModeEnabled = true;
+  } else {
+    stylesheet.href = 'styles.css';
+    darkModeEnabled = false;
+  }
 }
